8e2cae322817b0c60d9933bf10562053f75db430
benedikt.k <benedikt.k@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-06-22 09:11:34 +0000

    Removed files left in the root

--- make_tsensed.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# File name: make_anon.sh
-# Date:      2010-06-19 10:26
-# Author:    Kristján Rúnarsson
-
-g++ -I/usr/local/include/ -lgnutls -L/usr/local/lib/ TSenseDaemon.cpp BDaemon.cpp TlsServer.cpp -o tsensed
