08973978bb980d87bd779238671c8a3f02e03f4b
krunarsson <krunarsson@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-06-24 12:34:55 +0000

    Added OpenSSL based implementation of the Tsense daemon with with simple echo functionality over an anonymous TLS connection. Also added a matching example client from the OpenSSL sample code.

---
+++ trunk/server/OpenSslServer/TlsServer.h
@@ -0,0 +1,54 @@
+/*
+   File name: TlsServer.h
+   Date:      2010-06-21 11:28
+   Author:    Kristján Rúnarsson
+*/
+
+#ifndef __TLSSERVER_H__
+#define __TLSSERVER_H__
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#include <syslog.h>
+#include <errno.h>
+#include <gnutls/gnutls.h>
+#include <string.h>
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+/* This is a sample TLS 1.0 echo server, for anonymous authentication only.
+ */
+
+using namespace std;
+
+class TlsServer {
+    private:
+		void handleError(const char* msg);
+		void initOpenSSL();
+		SSL_CTX* setupCtx();
+		DH* setupDiffeHelleman();
+
+		int _listenPort;
+
+    public:
+		TlsServer(int listenPort);
+		void serverMain();
+};
+
+
+#endif
