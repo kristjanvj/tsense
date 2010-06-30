692396335ba56445f11868043b10a1de48de83c1
kristjanvj <kristjanvj@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-06-30 23:43:47 +0000

    added defines for t-table options

--- trunk/aes-crypt/arduino_aes/environment.h
+++ trunk/aes-crypt/arduino_aes/environment.h
@@ -1,6 +1,11 @@
 
 
-//#define __ARDUINO__DUEMILANOVE__
-#define __INTEL_32_LINUX__
+#define __ARDUINO__DUEMILANOVE__
+//#define __INTEL_32_LINUX__
 //#define __INTEL32_BSD__
+
+//#define t_box_transform
+//#define t_table_generate
+#define unroll_encrypt_loop
+
 
