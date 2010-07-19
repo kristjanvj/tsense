b1bf6f47cf6e5a499645d41cd16e6ce7dbbed4d9
krunarsson <krunarsson@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-07-19 11:45:17 +0000

    Split cMAC code out of aes_crypt.cpp and added a aes_common.h file for definitions common to encryption, decryption and cmac code.

--- aes-crypt/non_ard_examples/make_aes_cmac_key_test.sh
+++ aes-crypt/non_ard_examples/make_aes_cmac_key_test.sh
@@ -4,6 +4,6 @@
 # Date:      2010-07-12 13:26
 # Author:    Kristján Rúnarsson
 
-g++ -Wall -D_INTEL_64 aes_cmac_key_test.cpp ../arduino_aes/aes_crypt.cpp -I ../arduino_aes/ -O2 -o aes_cmac_key_test
+g++ -Wall -D_INTEL_64 aes_cmac_key_test.cpp ../arduino_aes/aes_cmac.cpp ../arduino_aes/aes_crypt.cpp -I ../arduino_aes/ -O2 -o aes_cmac_key_test
 
 
