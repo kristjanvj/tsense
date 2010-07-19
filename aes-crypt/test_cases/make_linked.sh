18973b115dd182687ab93d842ad02bad6e1ee8fb
krunarsson <krunarsson@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-07-19 12:46:06 +0000

    Renamed aes encryption testcase.

--- aes-crypt/test_cases/make_linked.sh
+++
@@ -1,2 +0,0 @@
-#g++ -Wall -D_INTEL_32 aes_linked.cc ../arduino_aes/aes_crypt.cpp -I ../arduino_aes/ -O2 -o aes_linked
-g++ -Wall -D_INTEL_64 aes_linked.cc ../arduino_aes/aes_crypt.cpp -I ../arduino_aes/ -O2 -o aes_linked
