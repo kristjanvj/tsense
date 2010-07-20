1bf23001005653f4bcb6ccd6a331a5b1a38c74a3
benedikt.k <benedikt.k@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-07-20 18:53:14 +0000

    AES code merged into aes_crypt, environment.h, aes_decrypt.* deleted. Test cases for the block in FIPS Append. B. Tested.

--- trunk/aes-crypt/arduino_aes/aes_crypt.h
+++ trunk/aes-crypt/arduino_aes/aes_crypt.h
@@ -1,6 +1,6 @@
-/**
- *
- * Kristjan Valur Jonsson
+/*
+ * Kristjan Valur Jonsson,
+ * Benedikt Kristinsson
  *
  * AES Encryption Implementation. Ported quick-and-dirty to the Arduino 
  * from the aes Intel platform implementation
@@ -9,53 +9,72 @@
  */
  
 #include <string.h>
-#include <math.h>
-#include "aes_common.h"
+//#include <math.h> // ?
 
 #ifndef __AES_CRYPT_H__
 #define __AES_CRYPT_H__
 
-/* These switches control the use of t-table transformations v.s. the 8-bit 
- * version as used in the textbook and various other sources. T-tables can be 
- * generated on startup or loaded from the pre-generated definitions. See the
- * initialization for the latter case in the code.
- */
-//#define t_box_transform  // 32 bit optimization, else vanilla 8 bit.
-//#define t_table_generate
+// Assume Arduino Duemilanove if we are compiling without the -D flag. 
+#if !(defined(_INTEL_32) || defined(_INTEL_64))
+// Defines for the arduino platform
+#define _ARDUINO_DUEMILANOVE         
+#define unroll_encrypt_loop
+#endif
 
 //#define unroll_encrypt_loop 
-
 //#define verbose_debug
+
+// typedef's depending on platform
+#ifdef _ARDUINO_DUEMILANOVE
+typedef unsigned char       byte_ard;
+typedef int                 int16_ard;
+typedef unsigned int        u_int16_ard;
+typedef long                int32_ard;
+typedef unsigned long       u_int32_ard;
+#endif
+#ifdef  _INTEL_32
+typedef unsigned char       byte_ard;
+typedef short               int16_ard;
+typedef unsigned short      u_int16_ard;
+typedef int                 int32_ard;
+typedef unsigned int        u_int32_ard;
+typedef long long           int64_ard;
+typedef unsigned long long  u_int64_ard;
+#endif
+#ifdef  _INTEL_64
+typedef unsigned char       byte_ard;
+typedef short               int16_ard;
+typedef unsigned short      u_int16_ard;
+typedef int                 int32_ard;
+typedef unsigned int        u_int32_ard;
+typedef long                int64_ard;
+typedef unsigned long       u_int64_ard;
+#endif
 
 // Some defines to aid code readability
 #define KEY_BYTES 16
-#define KEY_WORDS 4
-#define ROUNDS 10
-// #define BLOCK_BYTE_SIZE 16 // Moved to aes_common.h, it is used by crypt and cmac.
+#define KEY_WORDS 4 // Nb
+#define ROUNDS 10   // Nr
+#define BLOCK_BYTE_SIZE 16 
 
 extern unsigned char pKey[]; // The secret encryption key
 extern unsigned char pKeys[KEY_BYTES*12];
 
+// Common
 void KeyExpansion(const void *key, void *keys);
+void AddRoundKey(void *pText, const u_int32_ard *pKeys, int round);
 
-void addRoundKey(void *pText, const u_int32_ard *pKeys, int round);
-
-#ifndef t_box_transform
+// Encryption
 void subAndShift(void *pText);
-
 void mixColumns(void *pText);
-
-void ttransform(void *pText, const u_int32_ard *pKeys, int round);
-
-void lttransform(void *pText, const u_int32_ard *pKeys, int round);
-#endif
-
 #define ntransform(text,keys,round) subAndShift(text);mixColumns(text);addRoundKey(text,keys,round);
+void EncryptBlock(void *pText, const u_int32_ard *pKeys);
 
-void encryptBlock(void *pText, const u_int32_ard *pKeys);
+// Decryption
+void InvSubAndShift(void *pText);
+void InvMixColumns(void *pText);
+void DecryptBlock(void* pEncrypted, const u_int32_ard *pKeys);
 
-#if defined(t_box_transform) && defined(t_table_generate)
-void initializeTboxes();
-#endif
 
-#endif
+
+#endif  //__AES_CRYPT_H__
---
+++ trunk/aes-crypt/test_cases/block_d.cpp
@@ -0,0 +1,64 @@
+// Benedikt Kristinsson
+// Deciphers the example block in FIPS 197 Appendix B. 
+
+#include "aes_crypt.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define BLOCK_SIZE 16
+#define print_key_schedule
+
+
+// printBytes2 KVJ
+void printBytes2(unsigned char* pBytes, unsigned long dLength, int textWidth=16)
+{	
+	int bytecount=0;
+	for(unsigned long i=0;i<dLength;i++)
+	{
+		printf("%.2x ",pBytes[i]);
+		if ( ++bytecount == textWidth )
+		{
+			printf("\n");
+			bytecount=0;
+		}
+	}
+	if ( bytecount != 0 )
+		printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+  // Key and 16 byte block as per FIPS 197, Appendix B (Cipher Example)
+
+  byte_ard pKey[] = {
+    0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c 
+  };
+
+  // The ciphertext according to the FIPS and block_e.cpp
+  byte_ard pCipherBlock[] = {
+    0x39, 0x25, 0x84, 0x1d, 0x02, 0xdc, 0x09, 0xfb, 0xdc, 0x11, 0x85, 0x97, 0x19, 0x6a, 0x0b, 0x32 
+  };
+
+  // Allocate memory and generate the key schedule
+  unsigned char pKeys[KEY_BYTES*12];
+  KeyExpansion(pKey, pKeys);
+
+  printf("\nCiphertext: \n");
+  printBytes2((unsigned char *)pCipherBlock, BLOCK_SIZE);
+
+  #ifdef print_key_schedule
+  printf("\nKey Schdedule: \n");
+  printBytes2((unsigned char *)pKeys, BLOCK_SIZE*11);
+  printf("\n");
+  #endif
+
+  // Decrypt the single block
+  DecryptBlock(pCipherBlock, (const u_int32_ard*)pKeys);
+  printf("\nPlaintext: \n");
+  printBytes2((unsigned char *)pCipherBlock, BLOCK_SIZE);
+  
+  return 0;
+}
