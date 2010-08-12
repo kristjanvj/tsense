8c375855fef68a05adc0178d61f8edf986c265ad
benedikt.k <benedikt.k@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-12 14:54:43 +0000

    Updated make bash scripts.

--- aes-crypt/test_cases/make_blocktest.sh
+++ aes-crypt/test_cases/make_blocktest.sh
@@ -1,3 +1,2 @@
-g++ -Wall -D_INTEL_32 block_e.cpp ../arduino_aes/aes_crypt.cpp -I ../arduino_aes/ -O2 -o block_e
-g++ -Wall -D_INTEL_32 block_d.cpp ../arduino_aes/aes_crypt.cpp -I ../arduino_aes/ -O2 -o block_d
-#g++ -Wall -D_INTEL_64 aes_linked.cc ../arduino_aes/aes_crypt.cpp -I ../arduino_aes/ -O2 -o aes_linked
+g++ -Wall -D_INTEL_32 block_e.cpp ../lib/aes_crypt.cpp -I ../lib/ -O2 -o block_e
+g++ -Wall -D_INTEL_32 block_d.cpp ../lib/aes_crypt.cpp -I ../lib/ -O2 -o block_d
