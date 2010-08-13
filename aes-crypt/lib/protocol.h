71c9215e1711c440a732d0c7e8d76756bd954c66
benedikt.k <benedikt.k@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-13 16:40:58 +0000

    Pack the message from A to S, containing the key. The unpack method for S, doesnt read the ciphertext because S doesnt have the key. Testing of the ciphertext failed epicly, but will be implemented in the unpack method for T

--- aes-crypt/lib/protocol.h
+++ aes-crypt/lib/protocol.h
@@ -16,18 +16,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* Some defines - Please take care if you change them. */
+/*
+  Some defines - Please take care if you change them.
+
+  The macros below depend on these sizes for easy calculation. If you ahvenge
+  NOUNCE_SIZE, TIMER_SIZE, MSGTYPE_SIZE, IDSIZE - please fix the macros below
+  accordingly. As is, this applies to the _PADLEN macros, since they use the
+  BLOCK_BYTE_SIZE without a dynamic multiplicator and will return minus values
+  if the _SIZE defines get larger. 
+*/
 #define ID_SIZE 6
 #define MSGTYPE_SIZE 1
 #define NOUNCE_SIZE 4
+#define TIMER_SIZE 4
 
-/* Some macros to calculate the various lengths of various things. */
-//#define IDMSG_SIZE ID_SIZE+NOUNCE_SIZE
-#define IDMSG_PADLEN BLOCK_SIZE - (ID_SIZE+NOUNCE_SIZE)
-#define IDMSG_CRYPTSIZE ID_SIZE + NOUNCE_SIZE + IDMSG_PADLEN
+/*
+  Some macros to calculate the various lengths of various things.
+
+  Please thread carefully if changing something. 
+*/
+#define IDMSG_PADLEN BLOCK_BYTE_SIZE - (ID_SIZE+NOUNCE_SIZE)
+#define IDMSG_CRYPTSIZE ID_SIZE + NOUNCE_SIZE + (IDMSG_PADLEN)
 #define IDMSG_FULLSIZE MSGTYPE_SIZE + ID_SIZE + IDMSG_CRYPTSIZE + BLOCK_BYTE_SIZE
 
-/* Structs */
+#define KEYTOSINK_PADLEN (BLOCK_BYTE_SIZE - (NOUNCE_SIZE + TIMER_SIZE))
+#define KEYTOSINK_CRYPTSIZE (NOUNCE_SIZE + KEY_BYTES + TIMER_SIZE + KEYTOSINK_PADLEN)
+#define KEYTOSINK_FULLSIZE MSGTYPE_SIZE + KEY_BYTES + TIMER_SIZE + KEYTOSINK_CRYPTSIZE + BLOCK_BYTE_SIZE
+
+/* Struct */
 struct message
 {
   byte_ard msgtype;
@@ -35,17 +51,19 @@ struct message
   byte_ard* pCipherID;
   u_int32_ard nounce;
   byte_ard cmac[BLOCK_BYTE_SIZE];
+  byte_ard* key;
+  byte_ard* ciphertext;
+  byte_ard* temp;
+  u_int32_ard timer;
 };
 
-void pack_idresponse(struct message* msg, const u_int32_ard* pKeys,
-                     void *pBuffer);
-void unpack_idresponse(void *pStream, const u_int32_ard* pKeys,
-                       struct message* msg);
+void pack_idresponse(struct message* msg, const u_int32_ard* pKeys, void *pBuffer);
+void unpack_idresponse(void *pStream, const u_int32_ard* pKeys, struct message* msg);
+
+void pack_keytosink(struct message* msg, const u_int32_ard* pKeys, void *pBuffer);
+void unpack_keytosink(void *pStream, struct message* msg);
 
 /* Cannot define IV here for some reason. Investigate */
 
-/* Misc methods to simplify code - will be moved */
-u_int32_ard padding (u_int32_ard strlen);
-u_int16_ard neededblocks(u_int32_ard len);
 
 #endif
