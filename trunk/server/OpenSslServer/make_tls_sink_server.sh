693e8980408ee293f0b22739d6cd88acbbc65907
krunarsson <krunarsson@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-14 10:57:03 +0000

    Deleted old make scripts.

--- trunk/server/OpenSslServer/make_tls_sink_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# File name: make_anon.sh
-# Date:      2010-06-19 10:26
-# Author:    Kristján Rúnarsson
-
-g++ -I/usr/include/ -I../common/  -lssl -lcrypto -L/usr/lib/ tssinkdaemon.cpp ../common/BDaemon.cpp tls_baseserver.cpp tls_sinkserver.cpp -o tssinkd
