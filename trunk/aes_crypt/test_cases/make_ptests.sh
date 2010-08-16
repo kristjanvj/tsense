91d5190f578b0192a5be8251b0c12ef06f64e8d2
krunarsson <krunarsson@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-16 11:12:35 +0000

    Minor fix in protocol tests case.

--- trunk/aes_crypt/test_cases/make_ptests.sh
+++ trunk/aes_crypt/test_cases/make_ptests.sh
@@ -1,1 +1,2 @@
-g++ -Wall -D_INTEL_32 protocol_tests.cpp ../lib/aes_crypt.cpp ../lib/protocol.cpp ../lib/aes_cmac.cpp -I ../lib/ -O2 -o ptests
+#g++ -Wall -D_INTEL_32 protocol_tests.cpp ../lib/aes_crypt.cpp ../lib/protocol.cpp ../lib/aes_cmac.cpp -I ../lib/ -O2 -o ptests
+g++ -Wall -D_INTEL_64 protocol_tests.cpp ../lib/aes_crypt.cpp ../lib/protocol.cpp ../lib/aes_cmac.cpp -I ../lib/ -O2 -o ptests
