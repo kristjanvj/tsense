0cf1c13a698f664c08b559074af5229e1f85eed3
krunarsson <krunarsson@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-17 10:43:59 +0000

    Minor fix to make_links.sh

--- trunk/tsensor/make_links.sh
+++ trunk/tsensor/make_links.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ln -s ../aes-crypt/lib/aes_cmac.cpp .
-ln -s ../aes-crypt/lib/aes_cmac.h .
-ln -s ../aes-crypt/lib/aes_crypt.cpp .
-ln -s ../aes-crypt/lib/aes_crypt.h .
+ln -s ../aes_crypt/lib/aes_cmac.cpp .
+ln -s ../aes_crypt/lib/aes_cmac.h .
+ln -s ../aes_crypt/lib/aes_crypt.cpp .
+ln -s ../aes_crypt/lib/aes_crypt.h .
