055e7167a7389abdb44f6c9b8338b9fb76453af0
kristjanvj <kristjanvj@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-18 21:39:35 +0000

    updated links for arduino code

--- tsensor/make_links.sh
+++ tsensor/make_links.sh
@@ -4,3 +4,6 @@ ln -s ../aes_crypt/lib/aes_cmac.cpp .
 ln -s ../aes_crypt/lib/aes_cmac.h .
 ln -s ../aes_crypt/lib/aes_crypt.cpp .
 ln -s ../aes_crypt/lib/aes_crypt.h .
+ln -s ../aes_crypt/lib/protocol.cpp .
+ln -s ../aes_crypt/lib/protocol.h .
+ln -s ../aes_crypt/lib/tstypes.h .
