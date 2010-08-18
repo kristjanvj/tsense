07adf6cf22a041307dce52bd4c14e20cf73e8d46
kristjanvj <kristjanvj@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-18 21:41:28 +0000

    temporary cleanup. INTEGRATE THIS FILE INTO PROTOCOL.H AT SOME POINT

--- tsensor/devinfo.h
+++ tsensor/devinfo.h
@@ -2,8 +2,12 @@
 #ifndef __ard_dev_info_H__
 #define __ard_dev_info_H__
 
-//#include <avr/pgmspace.h> // For the PROGMEM modifier
+//
+// TODO: REMOVE THIS FILE -- INTEGRATE WITH protocol.h
+//
 
+
+// TODO: A chunk of this should be part of the protocol pack/unpack and so forth.
 #define ID_BIT_SIZE    48
 #define TIME_BIT_SIZE  32
 #define LEN_BIT_SIZE    8
@@ -14,6 +18,7 @@
 #define VAL_BIT_SIZE    8
 
 #define ABITL 8
+
 
 #define INTERFACE_COUNT 2   // The number of sampled interfaces
 #define INTERFACE1 0
@@ -27,6 +32,7 @@
 // See: http://www.arduino.cc/en/Reference/PROGMEM
 // Algernatively, use the EEPROM. See http://www.arduino.cc/en/Reference/EEPROM
 
+/*
 char manName[] = {"TSG"};
 char modelName[] = {"ALPHA"};
 char serialNo[] = {"0000001"};
@@ -37,5 +43,6 @@ byte_ard masterKey[16] = {0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B
 
 byte_ard interface_types[INTERFACE_COUNT] = {INTERFACE1_TYPE,INTERFACE2_TYPE};
 char* interface_names[INTERFACE_COUNT] = {INTERFACE1_NAME,INTERFACE2_NAME};
+*/
 
 #endif /* __ard_dev_info_H__ */
