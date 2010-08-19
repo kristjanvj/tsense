d826030ec43583de6cac1a74d38ee54acad588a7
kristjanvj <kristjanvj@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-19 15:27:19 +0000

    fixed paths

--- trunk/aes_crypt/test_cases/make_aes_crypt_test.sh
+++ trunk/aes_crypt/test_cases/make_aes_crypt_test.sh
@@ -1,2 +1,2 @@
-#g++ -Wall -D_INTEL_32 aes_linked.cc ../arduino_aes/aes_crypt.cpp -I ../arduino_aes/ -O2 -o aes_linked
-g++ -Wall -D_INTEL_64 aes_crypt_test.cpp ../arduino_aes/aes_crypt.cpp -I ../arduino_aes/ -O2 -o aes_crypt_test
+g++ -Wall -D_INTEL_32 aes_crypt_test.cpp ../lib/aes_crypt.cpp -I ../lib/ -O2 -o aes_crypt_test
+#g++ -Wall -D_INTEL_64 aes_crypt_test.cpp ../lib/aes_crypt.cpp -I ../lib/ -O2 -o aes_crypt_test
