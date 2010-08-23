9a6643c8b3f92545138c1a9fec6e36001c2e8dee
benedikt.k <benedikt.k@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-23 23:50:13 +0000

    The generateKey method moved to a new file aes_utils.

---
+++ aes_crypt/lib/aes_utils.h
@@ -0,0 +1,10 @@
+/*
+ * aes_utils.cpp
+ *
+ * A basket for various utility methods used in the Tsense project
+ * regarding the AES crypto functions.  NOTE: Not part of the AES
+ * library and some methods are platform dependent. 
+ *
+ */
+
+int generateKey(byte_ard *newKey);
