d51e40a7654747755454b2dd5a7fe2490f046659
benedikt.k <benedikt.k@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-23 23:50:13 +0000

    The generateKey method moved to a new file aes_utils.

---
+++ trunk/aes_crypt/lib/aes_utils.cpp
@@ -0,0 +1,28 @@
+/*
+ * aes_utils.cpp
+ *
+ * A basket for various utility methods used in the Tsense project
+ * regarding the AES crypto functions.  NOTE: Not part of the AES
+ * library and some methods are platform dependent. 
+ *
+ */
+
+
+int generateKey(byte_ard *newKey)
+{
+	byte_ard res = 0;
+    u_int32_ard f;
+	FILE * urandom = fopen("/dev/urandom","r");
+
+	if(urandom)
+    {
+		for(u_int32_ard i=0; i<BLOCK_BYTE_SIZE; i++)
+        {
+			f = fread(&res, 1, sizeof(res), urandom);
+			newKey[i] = res;
+		}
+		fclose(urandom);
+		return 1;
+	}
+	return res;
+}
