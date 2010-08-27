f872f2e8426877e9f09a5a84e6fa7c6c816482e0
krunarsson <krunarsson@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-27 20:13:15 +0000

    Minor modifications to aes_utls.*.

--- aes_crypt/lib/aes_utils.h
+++ aes_crypt/lib/aes_utils.h
@@ -2,10 +2,11 @@
  * aes_utils.cpp
  *
  * A basket for various utility methods used in the Tsense project
- * regarding the AES crypto functions.  NOTE: Not part of the AES
- * library and some methods are platform dependent. 
+ * regarding the AES crypto functions.  NOTE: Some methods are platform 
+ * dependent. 
  *
  */
 #include "aes_crypt.h"
 
 int generateKey(byte_ard *newKey);
+int generateKeyOfLength(byte_ard *newKey, int length);
