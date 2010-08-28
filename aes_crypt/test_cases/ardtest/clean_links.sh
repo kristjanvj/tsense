e1f2e53cd675cf9ce545c9c07f15ac3816693124
kristjanvj <kristjanvj@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-28 11:49:41 +0000

    Added arduino test cases for encryption/decyption and MAC

---
+++ aes_crypt/test_cases/ardtest/clean_links.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm aes_cmac.cpp
+rm aes_cmac.h
+rm aes_crypt.cpp
+rm aes_crypt.h
+rm protocol.cpp
+rm protocol.h
+rm tstypes.h
+rm edevdata.h
---
+++ aes_crypt/test_cases/ardtest/make_links.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ln -s ../../lib/aes_cmac.cpp .
+ln -s ../../lib/aes_cmac.h .
+ln -s ../../lib/aes_crypt.cpp .
+ln -s ../../lib/aes_crypt.h .
+ln -s ../../lib/protocol.cpp .
+ln -s ../../lib/protocol.h .
+ln -s ../../lib/tstypes.h .
+ln -s ../../../tsensor/edevdata.h .
