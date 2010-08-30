f44a2040837e426a025b7fae13b1f3604a0b19c5
kristjanvj <kristjanvj@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-08-30 15:01:32 +0000

    Copied from OpenSslServer w. minor changes. Obsolete in server, required in tsensor

---
+++ tsensor/tsense_keypair.h
@@ -0,0 +1,28 @@
+/*
+   File name: tsense_keystore.h
+   Date:      2010-08-21 11:23
+   Author:    Kristjan Runarsson, Kristjan V. Jonsson
+*/
+
+#ifndef __TSENSE_KEYSTORE_H__
+#define __TSENSE_KEYSTORE_H__
+
+#include "aes_crypt.h"
+#include "aes_cmac.h"
+
+class TSenseKeyPair {
+private:
+	byte_ard cryptoKey[BLOCK_BYTE_SIZE];
+	byte_ard cryptoKeySched[BLOCK_BYTE_SIZE*11];
+	byte_ard macKey[BLOCK_BYTE_SIZE];
+	byte_ard macKeySched[BLOCK_BYTE_SIZE*11];
+
+public:
+	TSenseKeyPair(byte_ard * key, byte_ard *constant);
+	byte_ard * getCryptoKey();
+	byte_ard * getCryptoKeySched();
+	byte_ard * getMacKey();
+	byte_ard * getMacKeySched();
+};
+
+#endif
