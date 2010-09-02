dccd79381c8903bb12c51abb5af314a537c4ac86
krunarsson <krunarsson@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-09-02 21:16:18 +0000

    Header and source files containing constants for key derivation.

--- trunk/aes_crypt/lib/aes_utils.h
+++ trunk/aes_crypt/lib/aes_utils.h
@@ -6,7 +6,9 @@
  * dependent. 
  *
  */
+#include "string.h"
 #include "aes_crypt.h"
 
 int generateKey(byte_ard *newKey);
 int generateKeyOfLength(byte_ard *newKey, int length);
+void  printByteArd(unsigned char* pBytes, unsigned long dLength, int textWidth);
