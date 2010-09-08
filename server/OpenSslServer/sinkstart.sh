8e83e78075cabb349f402be5b05a9a3f18112969
krunarsson <krunarsson@1a64faec-f967-4189-d786-903e0bfeb31b>
2010-09-08 16:37:00 +0000

    Options for sinkstart script.

--- server/OpenSslServer/sinkstart.sh
+++ server/OpenSslServer/sinkstart.sh
@@ -1,13 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 # File name: sinkstart.sh
 # Date:      2010-09-08 14:04
 # Author:    
 
-./tssinkd \
+aPort=""
+sPort=""
+
+usage() {
+	echo "Usage: sinkstart.sh -a <authport> -s <sinkport>" \
+	1>&2; exit 1;
+}
+
+if [ "$#" -le 0 ]
+then   # Script needs at least one command-line argument.
+	usage
+fi  
+
+while getopts "a:s:h" flag
+	do
+    	case $flag in
+		a) aPort=$OPTARG;;
+		s) sPort=$OPTARG;;
+		h) usage;;
+		?) usage;;
+	esac
+done
+
+echo ./tssinkd \
 	--workdir /home/kr/tsense/server/OpenSslServer/ \
 	--lockdir /home/kr/tsense/server/OpenSslServer/ \
 	--auaddr auth.tsense.sudo.is \
-	--auport 6001 \
+	--auport $aPort \
 	--addr sink.tsense.sudo.is \
-	--port 6002
+	--port $sPort 
